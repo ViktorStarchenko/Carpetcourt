84fc6ba34a457b2d7fdd7552fd1f8fb8be7a4fa4
Nikolay Kasperovich <nikolai@raisetech.net>
2020-08-25 12:42:24 +0300

    meta descrpt

--- wp-content/themes/carpet-court/meta-description-script.php
+++ wp-content/themes/carpet-court/meta-description-script.php
@@ -10,7 +10,7 @@ for($page=1; $page < 20; $page++) {
 	$products = wc_get_products($args);
 	if (!empty($products)) {
 			foreach ($products as $product) {
-				$format = 'View %s online at Carpet Court. Find the perfect floor for your next project. Free measure & quote available.';
+				$format = 'View the %s online at Carpet Court. Find the perfect floor for your next project. Free measure & quote available.';
 				$new_meta_description = sprintf($format, $product->name);
 				update_post_meta($product->id, '_yoast_wpseo_metadesc', $new_meta_description);
 			}
